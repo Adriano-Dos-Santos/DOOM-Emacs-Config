9f2733e2b8023988d7c5abbe0c55cdf8a8a207f6
Adriano Dos Santos <adsc999@gmail.com>
2022-06-01 09:57:56 -0300

    01/06/22

--- config.el
+++ config.el
@@ -158,7 +158,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-dracula)
+(setq doom-theme 'doom-palenight)
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
 ;;MODELINE
